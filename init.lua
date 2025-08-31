e804b4c58c1fc360963d5033b60bbb6dfa9f428e
Dmitrii Maslov <dmitrii.maslov@udv.group>
2025-08-31 17:18:58 +0500

    Сказ о том как я хрен на структуру забил и решил потом отрефачить

--- init.lua
+++ init.lua
@@ -1,25 +1,65 @@
--- Bootstrap lazy.nvim
+-- bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+-- плагины
+require("lazy").setup({
+  -- Telescope для поиска файлов
+  { 
+    "nvim-telescope/telescope.nvim", 
+    dependencies = { "nvim-lua/plenary.nvim" } 
+  },
 
--- Setup lazy.nvim
-require("lazy").setup("plugins")
+  -- Telescope file browser
+  { 
+    "nvim-telescope/telescope-file-browser.nvim", 
+    dependencies = { "nvim-telescope/telescope.nvim" } 
+  },
+})
+
+-- пробел как leader
+vim.g.mapleader = " "
+
+-- базовые настройки
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4 
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+
+-- Telescope keymap
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope file_browser<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>fp',
+  ":Telescope file_browser path=%:p:h<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Кеймап: открыть file browser относительно корня проекта (git root)
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>fg',
+  ":Telescope file_browser path=%:p:h cwd=$(git rev-parse --show-toplevel)<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Кеймап: открыть file browser относительно открытой директории в vim
+vim.api.nvim_set_keymap(
+  'n',
+  '<leader>fr',
+  ":Telescope file_browser path=%:p:h cwd=vim.loop.cwd()<CR>",
+  { noremap = true, silent = true }
+)
